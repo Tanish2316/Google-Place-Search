a969a40ae8d16509e2e97a0a821bbea87d5da3f3
Tanish Suthar <tanish.sce21@sot.pdpu.ac.in>
2025-12-05 18:26:55 +0530

    fix api issue

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const CryptoJS = require("crypto-js");
 require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const secretKey = process.env.SECRET_KEY || "default_secret_key";
+const encrypted = process.env.GOOGLE_API_KEY;
+
+const GOOGLE_API_KEY = CryptoJS.AES.decrypt(encrypted, secretKey).toString(
+  CryptoJS.enc.Utf8
+);
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +26,7 @@ app.post("/search", async (req, res) => {
     {
       headers: {
         "Content-Type": "application/json",
-        "X-Goog-Api-Key": process.env.GOOGLE_API_KEY,
+        "X-Goog-Api-Key": GOOGLE_API_KEY,
         "X-Goog-FieldMask":
           "places.displayName,places.formattedAddress,places.location,places.addressComponents",
       },
